a1c7a0685c966c7d3773f2077ea53706290b6829
Christoffer Lindahl <christoffer@kekos.se>
2015-12-13 01:30:08 +0100

    Added method for setting table alias after setting table name.

--- src/QueryBuilders/VerbBase.php
+++ src/QueryBuilders/VerbBase.php
@@ -4,8 +4,8 @@
  *
  * VerbBase class
  *
- * @version 1.0
- * @date 2015-12-06
+ * @version 1.1
+ * @date 2015-12-13
  */
 
 namespace QueryBuilder\QueryBuilders;
@@ -20,6 +20,16 @@ abstract class VerbBase {
   public function __construct($table_name, $adapter) {
     $this->table_name = $table_name;
     $this->adapter = $adapter;
+  }
+
+  public function alias($alias) {
+    if (is_array($this->table_name)) {
+      $this->table_name[1] = $alias;
+    } else {
+      $this->table_name = array($this->table_name, $alias);
+    }
+
+    return $this;
   }
 }
 ?>

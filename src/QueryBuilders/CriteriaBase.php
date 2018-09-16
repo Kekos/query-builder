68303f9cb4f89b947e0f2d536437ab891e8a1475
Christoffer Lindahl <christoffer@kekos.se>
2018-09-16 18:27:49 +0200

    Removed duplicated code in Select from CriteriaBase

--- src/QueryBuilders/CriteriaBase.php
+++ src/QueryBuilders/CriteriaBase.php
@@ -14,25 +14,25 @@ abstract class CriteriaBase extends VerbBase
 {
     protected $where = [];
 
-    public function where($key, $operator = null, $value = null, $joiner = 'AND')
+    public function where($key, $operator = null, $value = null, $joiner = 'AND'): self
     {
         $this->where[] = compact('key', 'operator', 'value', 'joiner');
         return $this;
     }
 
-    public function whereNot($key, $operator = null, $value = null)
+    public function whereNot($key, $operator = null, $value = null): self
     {
         $this->where($key, $operator, $value, 'AND NOT');
         return $this;
     }
 
-    public function whereOr($key, $operator = null, $value = null)
+    public function whereOr($key, $operator = null, $value = null): self
     {
         $this->where($key, $operator, $value, 'OR');
         return $this;
     }
 
-    public function whereOrNot($key, $operator = null, $value = null)
+    public function whereOrNot($key, $operator = null, $value = null): self
     {
         $this->where($key, $operator, $value, 'OR NOT');
         return $this;
--- src/QueryBuilders/Select.php
+++ src/QueryBuilders/Select.php
@@ -81,30 +81,6 @@ class Select extends CriteriaBase
         return $this;
     }
 
-    public function where($key, $operator = null, $value = null, $joiner = 'AND'): self
-    {
-        $this->where[] = compact('key', 'operator', 'value', 'joiner');
-        return $this;
-    }
-
-    public function whereNot($key, $operator = null, $value = null): self
-    {
-        $this->where($key, $operator, $value, 'AND NOT');
-        return $this;
-    }
-
-    public function whereOr($key, $operator = null, $value = null): self
-    {
-        $this->where($key, $operator, $value, 'OR');
-        return $this;
-    }
-
-    public function whereOrNot($key, $operator = null, $value = null): self
-    {
-        $this->where($key, $operator, $value, 'OR NOT');
-        return $this;
-    }
-
     public function groupby($columns): self
     {
         if (!is_array($columns)) {

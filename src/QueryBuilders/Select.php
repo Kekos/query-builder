b78ffd040d1c9d518c0484fdd25b8a0182fc06de
Christoffer Lindahl <christoffer@trinax.se>
2021-08-16 10:24:14 +0200

    Removed duplicated code in Select from CriteriaBase
    Cherry-picked from 68303f9cb4f89b947e0f2d536437ab891e8a1475

--- src/QueryBuilders/Select.php
+++ src/QueryBuilders/Select.php
@@ -81,30 +81,6 @@ class Select extends CriteriaBase
         return $this;
     }
 
-    public function where($key, $operator = null, $value = null, $joiner = 'AND')
-    {
-        $this->where[] = compact('key', 'operator', 'value', 'joiner');
-        return $this;
-    }
-
-    public function whereNot($key, $operator = null, $value = null)
-    {
-        $this->where($key, $operator, $value, 'AND NOT');
-        return $this;
-    }
-
-    public function whereOr($key, $operator = null, $value = null)
-    {
-        $this->where($key, $operator, $value, 'OR');
-        return $this;
-    }
-
-    public function whereOrNot($key, $operator = null, $value = null)
-    {
-        $this->where($key, $operator, $value, 'OR NOT');
-        return $this;
-    }
-
     public function groupby($columns)
     {
         if (!is_array($columns)) {

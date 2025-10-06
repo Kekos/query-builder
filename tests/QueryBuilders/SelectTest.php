Kekos/query-builder

commit cbbef47930f30338e81dd348e0f973ebb90d1f04

Cloning an object of `Delete`, `Select` or `Update` now creates new empty WHERE and HAVING buffers to match behavior of pre version 1.8.0

src/QueryBuilders/Delete.php
@@ -27,6 +27,11 @@ class Delete extends VerbBase
         $this->where = new CriteriaBuilder($adapter);
     }
 
+    public function __clone()
+    {
+        $this->where = new CriteriaBuilder($this->adapter);
+    }
+
     private function sanitizeField($field)
     {
         if ($field instanceof Raw) {

src/QueryBuilders/Select.php
@@ -40,6 +40,12 @@ class Select extends VerbBase
         $this->having = new CriteriaBuilder($adapter);
     }
 
+    public function __clone()
+    {
+        $this->where = new CriteriaBuilder($this->adapter);
+        $this->having = new CriteriaBuilder($this->adapter);
+    }
+
     /**
      * Adds columns to select
      *

src/QueryBuilders/Update.php
@@ -29,6 +29,11 @@ class Update extends VerbBase
         $this->where = new CriteriaBuilder($adapter);
     }
 
+    public function __clone()
+    {
+        $this->where = new CriteriaBuilder($this->adapter);
+    }
+
     /**
      * @param array $values Column name as array key
      * @return Update

tests/QueryBuilders/DeleteTest.php
@@ -17,6 +17,14 @@ class DeleteTest extends TestCase
         $this->delete = new Delete('foo_join', new MySqlAdapter());
     }
 
+    public function testCloneWhere()
+    {
+        $cloned_delete = clone $this->delete;
+        $cloned_delete->where('foo', '=', 42);
+
+        $this->assertEmpty($this->delete->getWhere());
+    }
+
     public function testToSql()
     {
         $expected = [

tests/QueryBuilders/SelectTest.php
@@ -109,6 +109,16 @@ class SelectTest extends TestCase
         $this->assertEmpty($this->select->getOrderBy());
     }
 
+    public function testCloneWhereHaving()
+    {
+        $cloned_select = clone $this->select;
+        $cloned_select->where('foo', '=', 42);
+        $cloned_select->having('bar', '=', 1337);
+
+        $this->assertEmpty($this->select->getWhere());
+        $this->assertEmpty($this->select->getHaving());
+    }
+
     public function testLimit()
     {
         $this->select->limit(10);

tests/QueryBuilders/UpdateTest.php
@@ -18,6 +18,14 @@ class UpdateTest extends TestCase
         $this->update = new Update('foo', new MySqlAdapter());
     }
 
+    public function testCloneWhere()
+    {
+        $cloned_update = clone $this->update;
+        $cloned_update->where('foo', '=', 42);
+
+        $this->assertEmpty($this->update->getWhere());
+    }
+
     public function testToSql()
     {
         $expected = [

commit f637596a6c38ac5ea4e18ac8b1d4511ec6000d8e

Cloning an object of `Delete`, `Select` or `Update` now clones WHERE and HAVING buffers to match behavior of pre version 1.8.0
Regression from cbbef47930f30338e81dd348e0f973ebb90d1f04

src/QueryBuilders/Delete.php
@@ -29,7 +29,7 @@ class Delete extends VerbBase
 
     public function __clone()
     {
-        $this->where = new CriteriaBuilder($this->adapter);
+        $this->where = clone $this->where;
     }
 
     private function sanitizeField($field)

src/QueryBuilders/Select.php
@@ -42,8 +42,8 @@ class Select extends VerbBase
 
     public function __clone()
     {
-        $this->where = new CriteriaBuilder($this->adapter);
-        $this->having = new CriteriaBuilder($this->adapter);
+        $this->where = clone $this->where;
+        $this->having = clone $this->having;
     }
 
     /**

src/QueryBuilders/Update.php
@@ -31,7 +31,7 @@ class Update extends VerbBase
 
     public function __clone()
     {
-        $this->where = new CriteriaBuilder($this->adapter);
+        $this->where = clone $this->where;
     }
 
     /**

tests/QueryBuilders/DeleteTest.php
@@ -19,10 +19,26 @@ class DeleteTest extends TestCase
 
     public function testCloneWhere()
     {
-        $cloned_delete = clone $this->delete;
-        $cloned_delete->where('foo', '=', 42);
+        $where_test1 = [
+            'key' => 'test1',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
+        $where_foo = [
+            'key' => 'foo',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
 
-        $this->assertEmpty($this->delete->getWhere());
+        $this->delete->where($where_test1['key'], '=', $where_test1['value']);
+
+        $cloned_delete = clone $this->delete;
+        $cloned_delete->where($where_foo['key'], '=', $where_foo['value']);
+
+        $this->assertEquals([$where_test1], $this->delete->getWhere());
+        $this->assertEquals([$where_test1, $where_foo], $cloned_delete->getWhere());
     }
 
     public function testToSql()

tests/QueryBuilders/SelectTest.php
@@ -111,12 +111,42 @@ class SelectTest extends TestCase
 
     public function testCloneWhereHaving()
     {
-        $cloned_select = clone $this->select;
-        $cloned_select->where('foo', '=', 42);
-        $cloned_select->having('bar', '=', 1337);
+        $where_test1 = [
+            'key' => 'test1',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
+        $having_test2 = [
+            'key' => 'test2',
+            'operator' => '=',
+            'value' => 1337,
+            'joiner' => 'AND',
+        ];
+        $where_foo = [
+            'key' => 'foo',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
+        $having_bar = [
+            'key' => 'bar',
+            'operator' => '=',
+            'value' => 1337,
+            'joiner' => 'AND',
+        ];
 
-        $this->assertEmpty($this->select->getWhere());
-        $this->assertEmpty($this->select->getHaving());
+        $this->select->where($where_test1['key'], '=', $where_test1['value']);
+        $this->select->having($having_test2['key'], '=', $having_test2['value']);
+
+        $cloned_select = clone $this->select;
+        $cloned_select->where($where_foo['key'], '=', $where_foo['value']);
+        $cloned_select->having($having_bar['key'], '=', $having_bar['value']);
+
+        $this->assertEquals([$where_test1], $this->select->getWhere());
+        $this->assertEquals([$having_test2], $this->select->getHaving());
+        $this->assertEquals([$where_test1, $where_foo], $cloned_select->getWhere());
+        $this->assertEquals([$having_test2, $having_bar], $cloned_select->getHaving());
     }
 
     public function testLimit()

tests/QueryBuilders/UpdateTest.php
@@ -20,10 +20,26 @@ class UpdateTest extends TestCase
 
     public function testCloneWhere()
     {
-        $cloned_update = clone $this->update;
-        $cloned_update->where('foo', '=', 42);
+        $where_test1 = [
+            'key' => 'test1',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
+        $where_foo = [
+            'key' => 'foo',
+            'operator' => '=',
+            'value' => 42,
+            'joiner' => 'AND',
+        ];
 
-        $this->assertEmpty($this->update->getWhere());
+        $this->update->where($where_test1['key'], '=', $where_test1['value']);
+
+        $cloned_update = clone $this->update;
+        $cloned_update->where($where_foo['key'], '=', $where_foo['value']);
+
+        $this->assertEquals([$where_test1], $this->update->getWhere());
+        $this->assertEquals([$where_test1, $where_foo], $cloned_update->getWhere());
     }
 
     public function testToSql()